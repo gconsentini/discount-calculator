dea58a3cf83cbda245e3dcaa7804556cfd45c0c3
Gustavo Consentini <gustavoconsentini@gmail.com>
2021-05-18 02:50:36 -0300

    Swagger docs

--- products-api/src/main/java/com/gconsentini/productsapi/ProductsApiApplication.java
+++ products-api/src/main/java/com/gconsentini/productsapi/ProductsApiApplication.java
@@ -2,8 +2,10 @@ package com.gconsentini.productsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ProductsApiApplication {
 
 	public static void main(String[] args) {
